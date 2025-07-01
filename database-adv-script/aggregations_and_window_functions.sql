61333e160158ca2a7178d5f1a51c76af6ce0b942
MURINDABIGWI TRIBERT <tribertmuto@gmail.com>
2025-07-01 21:54:02 +0200

    Update aggregations_and_window_functions.sql

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -1,21 +1,17 @@
--- Step 1: Get total bookings per user
--- Step 2: Get total bookings and rank per property
--- Combine results in one query
-
-WITH PropertyBookings AS (
-    SELECT
-        property_id,
-        COUNT(*) AS total_bookings_per_property,
-        RANK() OVER (ORDER BY COUNT(*) DESC) AS booking_rank
-    FROM bookings
-    GROUP BY property_id
-),
-UserBookings AS (
+WITH UserBookings AS (
     SELECT
         user_id,
         COUNT(*) AS total_bookings_per_user
     FROM bookings
     GROUP BY user_id
+),
+PropertyBookings AS (
+    SELECT
+        property_id,
+        COUNT(*) AS total_bookings_per_property,
+        ROW_NUMBER() OVER (ORDER BY COUNT(*) DESC) AS booking_rank
+    FROM bookings
+    GROUP BY property_id
 )
 
 SELECT
@@ -33,4 +29,6 @@ GROUP BY
     pb.property_id,
     pb.total_bookings_per_property,
     pb.booking_rank
-ORDER BY ub.user_id, pb.booking_rank;
+ORDER BY
+    ub.user_id,
+    pb.booking_rank;
