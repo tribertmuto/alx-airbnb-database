3c64f1acc3b1b5c7e036dbe1a887bce1214cf65a
MURINDABIGWI TRIBERT <tribertmuto@gmail.com>
2025-07-01 21:14:53 +0200

    Update joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,24 +1,36 @@
--- Emulated FULL OUTER JOIN (for MySQL)
+-- 1. INNER JOIN: Get all bookings and their respective users
 SELECT 
-    users.user_id,
-    users.name,
-    bookings.booking_id,
-    bookings.property_id,
-    bookings.booking_date
+    b.booking_id,
+    b.property_id,
+    b.booking_date,
+    u.user_id,
+    u.username,
+    u.email
 FROM 
-    users
-LEFT JOIN 
-    bookings ON users.user_id = bookings.user_id
+    Bookings b
+INNER JOIN 
+    Users u ON b.user_id = u.user_id;
 
-UNION
-
+-- 2. LEFT JOIN: Get all properties and their reviews, including properties with no reviews
 SELECT 
-    users.user_id,
-    users.name,
-    bookings.booking_id,
-    bookings.property_id,
-    bookings.booking_date
+    p.property_id,
+    p.property_name,
+    r.review_id,
+    r.rating,
+    r.comment
 FROM 
-    bookings
+    Properties p
 LEFT JOIN 
-    users ON users.user_id = bookings.user_id;
+    Reviews r ON p.property_id = r.property_id;
+
+-- 3. FULL OUTER JOIN: Get all users and all bookings, even if no booking or no user match
+SELECT 
+    u.user_id,
+    u.username,
+    b.booking_id,
+    b.property_id,
+    b.booking_date
+FROM 
+    Users u
+FULL OUTER JOIN 
+    Bookings b ON u.user_id = b.user_id;
