ec4f824aacabf4abf2521edbad156fb43c25448f
MURINDABIGWI TRIBERT <tribertmuto@gmail.com>
2025-07-01 22:48:22 +0200

    Update perfomance.sql

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,5 +1,18 @@
--- performance.sql
-
+SELECT
+    b.booking_id,
+    b.booking_date,
+    u.user_id,
+    u.name AS user_name,
+    p.property_id,
+    p.name AS property_name,
+    pay.payment_id,
+    pay.amount,
+    pay.payment_date
+FROM
+    bookings b
+INNER JOIN users u ON b.user_id = u.user_id
+INNER JOIN properties p ON b.property_id = p.property_id
+INNER JOIN payments pay ON b.payment_id = pay.payment_id;
 SELECT
     b.booking_id,
     b.booking_date,
